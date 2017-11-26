cd01ddaacad995c5c3560108b06318ad36b6aded
Shah Newaj Rabbi Shishir <shahshishir.09@gmail.com>
2017-11-26 21:43:56 +0600

    Add files via upload

---
+++ 4A._Watermelon[1].cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main ()
+{
+    int n;
+
+    cin >> n;
+
+    if ((n % 2 == 0) && n != 2)
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
+
+	return 0;
+}
