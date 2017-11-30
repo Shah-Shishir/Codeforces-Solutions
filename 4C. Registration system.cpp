b6cc9cbdc8d60996b69ce87d07a56a33c03e8b59
Shah Newaj Rabbi Shishir <shahshishir.09@gmail.com>
2017-12-01 04:28:45 +0600

    Add files via upload

---
+++ 4C. Registration system.cpp
@@ -0,0 +1,69 @@
+/***
+
+            Bismillahir Rahmanir Rahim
+            Read the name of Allah, who created you!!!
+            Author : Shah Newaj Rabbi Shishir
+            Department of CSE, City University, Bangladesh.
+
+***/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define sf scanf
+#define pf printf
+#define ssf sscanf
+#define spf sprintf
+#define fsf fscanf
+#define fpf fprintf
+#define fast ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+#define scase sf ("%d",&tc)
+#define sn sf ("%d",&n)
+#define whilecase while (tc--)
+#define eof while (cin >> n)
+#define forloop for (pos=1; pos<=tc; pos++)
+#define arrayloop (i=0; i<n; i++)
+#define cinstr cin >> str
+#define getstr getline (cin,str)
+#define pcase pf ("Case %d: ",pos)
+#define pii pair <int,int>
+#define pb push_back
+#define in insert
+#define llu unsigned long long
+#define lld long long
+#define U unsigned int
+#define endl "\n"
+
+const int MOD = 1000000007;
+const int MAX = 1000005;
+
+int main (void)
+{
+    /*
+    freopen ("input.txt","r",stdin);
+    freopen ("output.txt","w",stdout);
+    */
+
+    int n,i;
+    char str[35];
+
+    while (sf ("%d",&n) != EOF)
+    {
+        getchar ();
+
+        map <string,int> mp;
+
+        for (i=1; i<=n; i++)
+        {
+            sf ("%s",str);
+            mp[str]++;
+
+            if (mp[str] == 1)
+                pf ("OK\n");
+            else
+                pf ("%s%d\n",str,mp[str]-1);
+        }
+    }
+
+    return 0;
+}
