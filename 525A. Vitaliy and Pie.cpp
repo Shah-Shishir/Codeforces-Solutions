854b3d6d4babdc9343c3f3eaff0bda4b6f8db510
Shah Newaj Rabbi Shishir <shahshishir.09@gmail.com>
2017-12-01 21:28:51 +0600

    Add files via upload

---
+++ 525A. Vitaliy and Pie.cpp
@@ -0,0 +1,80 @@
+/***
+
+            Bismillahir Rahmanir Rahim
+            Read the name of Allah, who created you!!!
+            Author : Shah Newaj Rabbi Shishir
+            Department of CSE, City University, Bangladesh.
+
+***/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define sf scanf
+#define pf printf
+#define ssf sscanf
+#define spf sprintf
+#define fsf fscanf
+#define fpf fprintf
+#define fast ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+#define scase sf ("%d",&tc)
+#define sn sf ("%d",&n)
+#define whilecase while (tc--)
+#define eof while (cin >> n)
+#define forloop for (pos=1; pos<=tc; pos++)
+#define arrayloop (i=0; i<n; i++)
+#define cinstr cin >> str
+#define getstr getline (cin,str)
+#define pcase pf ("Case %d: ",pos)
+#define pii pair <int,int>
+#define pb push_back
+#define in insert
+#define llu unsigned long long
+#define lld long long
+#define U unsigned int
+#define endl "\n"
+
+const int MOD = 1000000007;
+const int MAX = 1000005;
+
+int main (void)
+{
+    /*
+    freopen ("input.txt","r",stdin);
+    freopen ("output.txt","w",stdout);
+    */
+
+    int n,i,ans;
+    char str[200000],x;
+
+    while (sf ("%d",&n) != EOF)
+    {
+        n = 2*n-2;
+
+        sf ("%s",str);
+
+        map <char,int> mp;
+        ans = 0;
+
+        for (i=0; i<n; i++)
+        {
+            x = str[i];
+
+            if (islower(x))
+                mp[x]++;
+            else
+            {
+                x = tolower(x);
+
+                if (mp[x] > 0)
+                    --mp[x];
+                else
+                    ++ans;
+            }
+        }
+
+        pf ("%d\n",ans);
+    }
+
+    return 0;
+}
