fa71d5e7a9f6969c528115fc08af819cc9816820
Shah Newaj Rabbi Shishir <shahshishir.09@gmail.com>
2017-12-08 22:16:48 +0600

    Add files via upload

---
+++ 675A. Infinite Sequence.cpp
@@ -0,0 +1,71 @@
+/***
+
+            Bismillahir Rahmanir Rahim
+            Read the name of Allah, who created you!!!
+            Author : Shah Newaj Rabbi Shishir
+            Department of CSE, City University, Bangladesh.
+
+***/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define sf scanf
+#define pf printf
+#define ssf sscanf
+#define spf sprintf
+#define fsf fscanf
+#define fpf fprintf
+#define fast ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+#define scase sf ("%d",&tc)
+#define sn sf ("%d",&n)
+#define whilecase while (tc--)
+#define eof while (cin >> n)
+#define forloop for (pos=1; pos<=tc; pos++)
+#define arrayloop (i=0; i<n; i++)
+#define cinstr cin >> str
+#define getstr getline (cin,str)
+#define pcase pf ("Case %d: ",pos)
+#define pii pair <int,int>
+#define pb push_back
+#define in insert
+#define llu unsigned long long
+#define lld long long
+#define U unsigned int
+#define endl "\n"
+
+const int MOD = 1000000007;
+const int MAX = 1000005;
+
+int main (void)
+{
+    /*
+    freopen ("input.txt","r",stdin);
+    freopen ("output.txt","w",stdout);
+    */
+
+    int a,b,c;
+    double n;
+
+    while (sf ("%d %d %d",&a,&b,&c) != EOF)
+    {
+        if (c == 0)
+        {
+            if (a == b)
+                pf ("YES\n");
+            else
+                pf ("NO\n");
+        }
+        else
+        {
+            n = ((b-a)/(double)(c))+1;
+
+            if (n <= 0 || n != (int)(n))
+                pf ("NO\n");
+            else
+                pf ("YES\n");
+        }
+    }
+
+    return 0;
+}
