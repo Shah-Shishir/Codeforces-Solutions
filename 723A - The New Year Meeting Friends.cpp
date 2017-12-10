51290696c301b7f1b66f942c9a6d1b71286cbf75
Shah Newaj Rabbi Shishir <shahshishir.09@gmail.com>
2017-12-11 03:22:39 +0600

    Add files via upload

---
+++ 723A - The New Year Meeting Friends.cpp
@@ -0,0 +1,57 @@
+/***
+
+            Bismillahir Rahmanir Rahim
+            Read the name of Allah, who created you!!!
+            Author : Shah Newaj Rabbi Shishir
+            Department of CSE, City University, Bangladesh.
+
+***/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define sf scanf
+#define pf printf
+#define ssf sscanf
+#define spf sprintf
+#define fsf fscanf
+#define fpf fprintf
+#define fast ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+#define scase sf ("%d",&tc)
+#define sn sf ("%d",&n)
+#define whilecase while (tc--)
+#define eof while (cin >> n)
+#define forloop for (pos=1; pos<=tc; pos++)
+#define arrayloop (i=0; i<n; i++)
+#define cinstr cin >> str
+#define getstr getline (cin,str)
+#define pcase pf ("Case %d: ",pos)
+#define pii pair <int,int>
+#define pb push_back
+#define in insert
+#define llu unsigned long long
+#define lld long long
+#define U unsigned int
+#define endl "\n"
+
+const int MOD = 1000000007;
+const int MAX = 1000005;
+
+int main (void)
+{
+    /*
+    freopen ("input.txt","r",stdin);
+    freopen ("output.txt","w",stdout);
+    */
+
+    int arr[3];
+
+    while (sf ("%d %d %d",&arr[0],&arr[1],&arr[2]) != EOF)
+    {
+        sort (arr,arr+3);
+
+        pf ("%d\n",(arr[2]-arr[1])+((arr[1]-arr[0])));
+    }
+
+    return 0;
+}
