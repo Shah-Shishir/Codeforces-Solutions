744196ea941152df57073ff0db7451e3fef2a597
Shah Newaj Rabbi Shishir <shahshishir.09@gmail.com>
2018-04-13 20:56:15 +0600

    Add files via upload

---
+++ 888A. Local Extrema.cpp
@@ -0,0 +1,64 @@
+/***
+
+            Bismillahir Rahmanir Rahim
+            Read the name of Allah, who created you!!!
+            Author : Shah Newaj Rabbi Shishir
+            Department of CSE, City University, Bangladesh.
+
+***/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define sf scanf
+#define pf printf
+#define ssf sscanf
+#define spf sprintf
+#define fsf fscanf
+#define fpf fprintf
+#define fast ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+#define scase sf ("%d",&tc)
+#define sn sf ("%d",&n)
+#define whilecase while (tc--)
+#define eof while (cin >> n)
+#define forloop for (pos=1; pos<=tc; pos++)
+#define arrayloop (i=0; i<n; i++)
+#define cinstr cin >> str
+#define getstr getline (cin,str)
+#define pcase pf ("Case %d: ",pos)
+#define pii pair <int,int>
+#define pb push_back
+#define in insert
+#define llu unsigned long long
+#define lld long long
+#define U unsigned int
+#define endl "\n"
+
+const int MOD = 1000000007;
+const int MAX = 1000005;
+
+int main (void)
+{
+    /*
+    freopen ("input.txt","r",stdin);
+    freopen ("output.txt","w",stdout);
+    */
+
+    int arr[1001],n,i,k;
+
+    while (sf ("%d",&n) != EOF)
+    {
+        for (i=0; i<n; i++)
+            sf ("%d",&arr[i]);
+
+        k = 0;
+
+        for (i=1; i<n-1; i++)
+            if ((arr[i] > arr[i-1] && arr[i] > arr[i+1]) || (arr[i] < arr[i-1] && arr[i] < arr[i+1]))
+                ++k;
+
+        pf ("%d\n",k);
+    }
+
+    return 0;
+}
