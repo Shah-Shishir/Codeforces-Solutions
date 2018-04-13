ccba904ed1f9c4c0728ff45bbd9fb1d79169f4a3
Shah Newaj Rabbi Shishir <shahshishir.09@gmail.com>
2018-04-13 19:56:41 +0600

    Add files via upload

---
+++ 6A - Triangle.cpp
@@ -0,0 +1,32 @@
+/***
+
+            Bismillahir Rahmanir Rahim
+            Read the name of Allah, who created you!!!
+            Author : Shah Newaj Rabbi Shishir
+            Department of CSE, City University, Bangladesh.
+
+***/
+
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+int main ()
+{
+    int a,b,c,d;
+
+    while (cin >> a >> b >> c >> d)
+    {
+        if ((a < b+c && b < a+c && c < a+b) || (a < b+d && b < a+d && d < a+b) || (a < d+c && d < a+c && c < a+d) || (d < b+c && b < d+c && c < d+b))
+            cout << "TRIANGLE" << endl;
+        else if ((a == b+c || b == a+c || c == a+b) || (a == b+d || b == a+d || d == a+b) || (a == d+c || d == a+c || c == a+d) || (d == b+c || b == d+c || c == d+b))
+            cout << "SEGMENT" << endl;
+        else
+            cout << "IMPOSSIBLE" << endl;
+    }
+
+    return 0;
+}
