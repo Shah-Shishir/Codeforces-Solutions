bcef1d20c4dbee5a305289b9512962a9b0208ff0
Shah Newaj Rabbi Shishir <shahshishir.09@gmail.com>
2018-04-13 20:56:08 +0600

    Add files via upload

---
+++ 892A. Greed.cpp
@@ -0,0 +1,74 @@
+/***
+
+            Bismillahir Rahmanir Rahim
+            Read the name of Allah, who created you!!!
+            Author : Shah Newaj Rabbi Shishir
+            Department of CSE, City University, Bangladesh.
+
+***/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define sf scanf
+#define pf printf
+#define ssf sscanf
+#define spf sprintf
+#define fsf fscanf
+#define fpf fprintf
+#define fast ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+#define scase sf ("%d",&tc)
+#define sn sf ("%d",&n)
+#define whilecase while (tc--)
+#define eof while (cin >> n)
+#define forloop for (pos=1; pos<=tc; pos++)
+#define arrayloop (i=0; i<n; i++)
+#define cinstr cin >> str
+#define getstr getline (cin,str)
+#define pcase pf ("Case %d: ",pos)
+#define pii pair <int,int>
+#define pb push_back
+#define in insert
+#define llu unsigned long long
+#define lld long long
+#define U unsigned int
+#define endl "\n"
+
+const int MOD = 1000000007;
+const int MAX = 1000005;
+
+int main (void)
+{
+    /*
+    freopen ("input.txt","r",stdin);
+    freopen ("output.txt","w",stdout);
+    */
+
+    lld n,i,vol,cap,sum,maxi;
+
+    while (sf ("%lld",&n) != EOF)
+    {
+        sum = 0;
+        lld arr[n];
+
+        for (i=0; i<n; i++)
+        {
+            sf ("%lld",&vol);
+            sum += vol;
+        }
+
+        for (i=0; i<n; i++)
+            sf ("%lld",&arr[i]);
+
+        sort (arr,arr+i);
+
+        maxi = arr[n-1]+arr[n-2];
+
+        if (maxi >= sum)
+            pf ("YES\n");
+        else
+            pf ("NO\n");
+    }
+
+    return 0;
+}
