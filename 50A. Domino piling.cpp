5496de81346b0279577eb3b107cc7e4b8b43718d
Shah Newaj Rabbi Shishir <shahshishir.09@gmail.com>
2018-04-13 21:10:49 +0600

    Add files via upload

---
+++ 50A. Domino piling.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main ()
+{
+	int m,n,product,quotient;
+
+	cin >> m >> n;
+
+	product = m*n;
+	quotient = product/2;
+
+	cout << quotient << endl;
+
+	return 0;
+}
