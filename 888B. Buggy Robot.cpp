c8a76fae2a469f2f0a6c15df21ec97b0846cb924
Shah Newaj Rabbi Shishir <shahshishir.09@gmail.com>
2018-04-13 20:56:22 +0600

    Add files via upload

---
+++ 888B. Buggy Robot.cpp
@@ -0,0 +1,72 @@
+/***
+
+            Bismillahir Rahmanir Rahim
+            Read the name of Allah, who created you!!!
+            Author : Shah Newaj Rabbi Shishir
+            Department of CSE, City University, Bangladesh.
+
+***/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define sf scanf
+#define pf printf
+#define ssf sscanf
+#define spf sprintf
+#define fsf fscanf
+#define fpf fprintf
+#define fast ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+#define scase sf ("%d",&tc)
+#define sn sf ("%d",&n)
+#define whilecase while (tc--)
+#define eof while (cin >> n)
+#define forloop for (pos=1; pos<=tc; pos++)
+#define arrayloop (i=0; i<n; i++)
+#define cinstr cin >> str
+#define getstr getline (cin,str)
+#define pcase pf ("Case %d: ",pos)
+#define pii pair <int,int>
+#define pb push_back
+#define in insert
+#define llu unsigned long long
+#define lld long long
+#define U unsigned int
+#define endl "\n"
+
+const int MOD = 1000000007;
+const int MAX = 1000005;
+
+int main (void)
+{
+    /*
+    freopen ("input.txt","r",stdin);
+    freopen ("output.txt","w",stdout);
+    */
+
+    int n,i,u,d,l,r,res;
+    char str[101];
+
+    while (sf ("%d %s",&n,str) != EOF)
+    {
+        u = d = l = r = 0;
+
+        for (i=0; i<n; i++)
+        {
+            if (str[i] == 'U')
+                ++u;
+            else if (str[i] == 'D')
+                ++d;
+            else if (str[i] == 'L')
+                ++l;
+            else
+                ++r;
+        }
+
+        res = 2 * (min (u,d)) + 2 * (min (l,r));
+
+        pf ("%d\n",res);
+    }
+
+    return 0;
+}
