9d1949907d8c74d7f24e869e9f87ab03c284ee1f
Shah Newaj Rabbi Shishir <shahshishir.09@gmail.com>
2018-06-01 21:05:55 +0600

    Add files via upload

---
+++ 988A. Diverse Team.cpp
@@ -0,0 +1,98 @@
+/***
+
+            Bismillahir Rahmanir Rahim
+            Read in the name of Allah, who created you!!!
+            Author : Shah Newaj Rabbi Shishir,
+            Department of CSE, City University, Bangladesh.
+
+***/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define sf scanf
+#define pf printf
+#define ssf sscanf
+#define spf sprintf
+#define fsf fscanf
+#define fpf fprintf
+#define fast ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+#define scase sf ("%d",&tc)
+#define sn sf ("%d",&n)
+#define whilecase while (tc--)
+#define eof while (cin >> n)
+#define forloop for (pos=1; pos<=tc; pos++)
+#define arrayloop (i=0; i<n; i++)
+#define cinstr cin >> str
+#define getstr getline (cin,str)
+#define pcase pf ("Case %d: ",pos)
+#define vi vector <int>
+#define pii pair <int,int>
+#define mii map <int,int>
+#define pb push_back
+#define in insert
+#define llu unsigned long long
+#define lld long long
+#define U unsigned int
+#define endl "\n"
+
+const int MOD = 1000000007;
+const int MAX = 1000005;
+
+int SetBit (int n, int x) { return n | (1 << x); }
+int ClearBit (int n, int x) { return n & ~(1 << x); }
+int ToggleBit (int n, int x) { return n ^ (1 << x); }
+bool CheckBit (int n, int x) { return (bool)(n & (1 << x)); }
+
+int arr[101];
+
+int main (void)
+{
+    /*
+    freopen ("input.txt","r",stdin);
+    freopen ("output.txt","w",stdout);
+    */
+
+    int n,k,i,val;
+
+    while (sf ("%d %d",&n,&k) != EOF)
+    {
+        map <int,bool> mp;
+        vector <int> v;
+        memset (arr,0,sizeof arr);
+
+        for (i=1; i<=n; i++)
+        {
+            sf ("%d",&val);
+            mp[val] = true;
+            ++arr[val];
+
+            if (arr[val] == 1)
+                v.pb(i);
+        }
+
+        if (mp.size() < k)
+            pf ("NO\n");
+        else
+        {
+            int len = v.size();
+
+            pf ("YES\n");
+
+            for (i=0; i<k; i++)
+            {
+                pf ("%d",v[i]);
+
+                if (i == k-1)
+                    pf ("\n");
+                else
+                    pf (" ");
+            }
+        }
+
+        mp.clear();
+        v.clear();
+    }
+
+    return 0;
+}
