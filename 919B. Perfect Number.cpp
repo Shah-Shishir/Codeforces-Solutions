43591e540969027a87797537abd14ff6bb1c5ce1
Shah Newaj Rabbi Shishir <shahshishir.09@gmail.com>
2018-06-07 00:30:05 +0600

    Add files via upload

---
+++ 919B. Perfect Number.cpp
@@ -0,0 +1,84 @@
+/***
+
+            Bismillahir Rahmanir Rahim
+            Read in the name of Allah, who created you!!!
+            Author : Shah Newaj Rabbi Shishir,
+            Department of CSE, City University, Bangladesh.
+
+***/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define sf scanf
+#define pf printf
+#define ssf sscanf
+#define spf sprintf
+#define fsf fscanf
+#define fpf fprintf
+#define fast ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+#define scase sf ("%d",&tc)
+#define sn sf ("%d",&n)
+#define whilecase while (tc--)
+#define eof while (cin >> n)
+#define forloop for (pos=1; pos<=tc; pos++)
+#define arrayloop (i=0; i<n; i++)
+#define cinstr cin >> str
+#define getstr getline (cin,str)
+#define pcase pf ("Case %d: ",pos)
+#define vi vector <int>
+#define pii pair <int,int>
+#define mii map <int,int>
+#define pb push_back
+#define in insert
+#define llu unsigned long long
+#define lld long long
+#define U unsigned int
+#define endl "\n"
+
+const int MOD = 1000000007;
+const int MAX = 1000005;
+
+int SetBit (int n, int x) { return n | (1 << x); }
+int ClearBit (int n, int x) { return n & ~(1 << x); }
+int ToggleBit (int n, int x) { return n ^ (1 << x); }
+bool CheckBit (int n, int x) { return (bool)(n & (1 << x)); }
+
+int sumOfDigits (int n)
+{
+    int sum = 0;
+
+    while (n)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+
+    return sum;
+}
+
+int main (void)
+{
+    /*
+    freopen ("input.txt","r",stdin);
+    freopen ("output.txt","w",stdout);
+    */
+
+    int k,i,cnt=0;
+
+    sf ("%d",&k);
+
+    for (i=19; i<11000000; i+=9)
+    {
+        if (sumOfDigits(i) == 10)
+        {
+            ++cnt;
+
+            if (cnt == k)
+            {
+                pf ("%d\n",i);
+                return 0;
+            }
+        }
+    }
+}
