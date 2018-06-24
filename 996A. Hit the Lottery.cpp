7f18033d9903dc40cdfbf4ed92aa2df420b89f65
Shah Newaj Rabbi Shishir <shahshishir.09@gmail.com>
2018-06-24 22:52:11 +0600

    Add files via upload

---
+++ 996A. Hit the Lottery.cpp
@@ -0,0 +1,77 @@
+/***
+
+            Bismillahir Rahmanir Rahim
+            Read in the name of Allah, who created you!!!
+            Author : Shah Newaj Rabbi Shishir,
+            Department of CSE, City University, Bangladesh.
+
+***/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define sf scanf
+#define pf printf
+#define ssf sscanf
+#define spf sprintf
+#define fsf fscanf
+#define fpf fprintf
+#define fast ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+#define scase sf ("%d",&tc)
+#define sn sf ("%d",&n)
+#define whilecase while (tc--)
+#define eof while (cin >> n)
+#define forloop for (pos=1; pos<=tc; pos++)
+#define arrayloop (i=0; i<n; i++)
+#define cinstr cin >> str
+#define getstr getline (cin,str)
+#define pcase pf ("Case %d: ",pos)
+#define vi vector <int>
+#define pii pair <int,int>
+#define mii map <int,int>
+#define pb push_back
+#define in insert
+#define llu unsigned long long
+#define lld long long
+#define U unsigned int
+#define endl "\n"
+
+const int MOD = 1000000007;
+const int MAX = 1000005;
+
+int SetBit (int n, int x) { return n | (1 << x); }
+int ClearBit (int n, int x) { return n & ~(1 << x); }
+int ToggleBit (int n, int x) { return n ^ (1 << x); }
+bool CheckBit (int n, int x) { return (bool)(n & (1 << x)); }
+
+int main (void)
+{
+    /*
+    freopen ("input.txt","r",stdin);
+    freopen ("output.txt","w",stdout);
+    */
+    int arr[] = {1,5,10,20,100};
+    int n,i,ans;
+
+    while (sf ("%d",&n) != EOF)
+    {
+        ans = 0;
+
+        for (i=4; i>=0; i--)
+        {
+            if (n >= arr[i])
+            {
+                ans += n/arr[i];
+
+                if (n % arr[i] == 0)
+                    break;
+                else
+                    n -= ((n / arr[i]) * arr[i]);
+            }
+        }
+
+        pf ("%d\n",ans);
+    }
+
+    return 0;
+}
