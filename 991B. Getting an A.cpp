4f1d2c104ca7f7fe3b93ba6bd8b2753052f9c30a
Shah Newaj Rabbi Shishir <shahshishir.09@gmail.com>
2018-06-24 20:57:01 +0600

    Add files via upload

---
+++ 991B. Getting an A.cpp
@@ -0,0 +1,89 @@
+/***
+
+            Bismillahir Rahmanir Rahim
+            Read in the name of Allah, who created you!!!
+            Author : Shah Newaj Rabbi Shishir,
+            Department of CSE, City University, Bangladesh.
+
+***/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define sf scanf
+#define pf printf
+#define ssf sscanf
+#define spf sprintf
+#define fsf fscanf
+#define fpf fprintf
+#define fast ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+#define scase sf ("%d",&tc)
+#define sn sf ("%d",&n)
+#define whilecase while (tc--)
+#define eof while (cin >> n)
+#define forloop for (pos=1; pos<=tc; pos++)
+#define arrayloop (i=0; i<n; i++)
+#define cinstr cin >> str
+#define getstr getline (cin,str)
+#define pcase pf ("Case %d: ",pos)
+#define vi vector <int>
+#define pii pair <int,int>
+#define mii map <int,int>
+#define pb push_back
+#define in insert
+#define llu unsigned long long
+#define lld long long
+#define U unsigned int
+#define endl "\n"
+
+const int MOD = 1000000007;
+const int MAX = 1000005;
+
+int SetBit (int n, int x) { return n | (1 << x); }
+int ClearBit (int n, int x) { return n & ~(1 << x); }
+int ToggleBit (int n, int x) { return n ^ (1 << x); }
+bool CheckBit (int n, int x) { return (bool)(n & (1 << x)); }
+
+int main (void)
+{
+    /*
+    freopen ("input.txt","r",stdin);
+    freopen ("output.txt","w",stdout);
+    */
+
+    int n,i,arr[105],sum,ans;
+    double avg;
+
+    while (sf ("%d",&n) != EOF)
+    {
+        sum = 0;
+
+        for (i=0; i<n; i++)
+        {
+            sf ("%d",&arr[i]);
+            sum += arr[i];
+        }
+
+        sort (arr,arr+i);
+
+        ans = 0;
+        avg = sum/(double)n;
+
+        for (i=0; i<n; i++)
+        {
+            if (avg >= 4.5)
+                break;
+            else
+            {
+                sum += (5-arr[i]);
+                arr[i] = 5;
+                avg = sum/(double)n;
+                ++ans;
+            }
+        }
+
+        pf ("%d\n",ans);
+    }
+
+    return 0;
+}
