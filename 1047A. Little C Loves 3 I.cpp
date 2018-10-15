e3fc6e21a16f6964223a6e1efa1c97a3d652c608
Shah Newaj Rabbi Shishir <shahshishir.09@gmail.com>
2018-10-16 01:51:47 +0600

    Add files via upload

---
+++ 1047A. Little C Loves 3 I.cpp
@@ -0,0 +1,54 @@
+/***
+
+            Bismillahir Rahmanir Rahim
+            Read in the name of Allah, who created you!!!
+            Author : Shah Newaj Rabbi Shishir,
+            Department of CSE, City University, Bangladesh.
+
+***/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ebar_khela_hoppe int main (void)
+#define bair_ho return 0
+#define sf scanf
+#define pf printf
+#define ssf sscanf
+#define spf sprintf
+#define fsf fscanf
+#define fpf fprintf
+#define fast ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+#define scase sf ("%d",&tc)
+#define sn sf ("%d",&n)
+#define whilecase while (tc--)
+#define eof while (cin >> n)
+#define forloop for (pos=1; pos<=tc; pos++)
+#define arrayloop (i=0; i<n; i++)
+#define cinstr cin >> str
+#define getstr getline (cin,str)
+#define pcase pf ("Case %d: ",pos)
+#define vi vector <int>
+#define pii pair <int,int>
+#define mii map <int,int>
+#define pb push_back
+#define in insert
+#define llu unsigned long long
+#define lld long long
+#define U unsigned int
+#define endl "\n"
+
+int main ()
+{
+    int n;
+
+    while (sf ("%d",&n) != EOF)
+    {
+        if (n % 3 == 0 || n % 3 == 1)
+            pf ("1 1 %d\n",n-2);
+        else
+            pf ("1 2 %d\n",n-3);
+    }
+
+    return 0;
+}
---
+++ 1047B. Cover Points.cpp
@@ -0,0 +1,59 @@
+/***
+
+            Bismillahir Rahmanir Rahim
+            Read in the name of Allah, who created you!!!
+            Author : Shah Newaj Rabbi Shishir,
+            Department of CSE, City University, Bangladesh.
+
+***/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ebar_khela_hoppe int main (void)
+#define bair_ho return 0
+#define sf scanf
+#define pf printf
+#define ssf sscanf
+#define spf sprintf
+#define fsf fscanf
+#define fpf fprintf
+#define fast ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+#define scase sf ("%d",&tc)
+#define sn sf ("%d",&n)
+#define whilecase while (tc--)
+#define eof while (cin >> n)
+#define forloop for (pos=1; pos<=tc; pos++)
+#define arrayloop (i=0; i<n; i++)
+#define cinstr cin >> str
+#define getstr getline (cin,str)
+#define pcase pf ("Case %d: ",pos)
+#define vi vector <int>
+#define pii pair <int,int>
+#define mii map <int,int>
+#define pb push_back
+#define in insert
+#define llu unsigned long long
+#define lld long long
+#define U unsigned int
+#define endl "\n"
+
+int main ()
+{
+    lld n,pos,a,b,ans;
+
+    while (sf ("%lld",&n) != EOF)
+    {
+        ans = 0;
+
+        for (pos=1; pos<=n; pos++)
+        {
+            sf ("%lld %lld",&a,&b);
+            ans = max (ans,a+b);
+        }
+
+        pf ("%lld\n",ans);
+    }
+
+    return 0;
+}
