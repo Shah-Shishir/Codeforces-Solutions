9bd7e5ae7ccfb6a8e3d0129121146c535efe393b
Shah Newaj Rabbi Shishir <shahshishir.09@gmail.com>
2018-10-18 03:54:59 +0600

    Add files via upload

---
+++ 327B. Hungry Sequence.cpp
@@ -0,0 +1,93 @@
+/***
+
+            Bismillahir Rahmanir Rahim
+            Read in the name of Allah, who created you!!!
+            Author : Shah Newaj Rabbi Shishir,
+            Department of CSE, City University, Bangladesh.
+
+***/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ebar_khela_hoppe int main (void)
+#define bair_ho return 0
+#define sf scanf
+#define pf printf
+#define ssf sscanf
+#define spf sprintf
+#define fsf fscanf
+#define fpf fprintf
+#define fast ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+#define scase sf ("%d",&tc)
+#define sn sf ("%d",&n)
+#define whilecase while (tc--)
+#define eof while (cin >> n)
+#define forloop for (pos=1; pos<=tc; pos++)
+#define arrayloop (i=0; i<n; i++)
+#define cinstr cin >> str
+#define getstr getline (cin,str)
+#define pcase pf ("Case %d: ",pos)
+#define vi vector <int>
+#define pii pair <int,int>
+#define mii map <int,int>
+#define pb push_back
+#define in insert
+#define llu unsigned long long
+#define lld long long
+#define U unsigned int
+#define endl "\n"
+
+const int MOD = 1000000007;
+const int MAX = 1500005;
+
+int SetBit (int n, int x) { return n | (1 << x); }
+int ClearBit (int n, int x) { return n & ~(1 << x); }
+int ToggleBit (int n, int x) { return n ^ (1 << x); }
+bool CheckBit (int n, int x) { return (bool)(n & (1 << x)); }
+
+bool prime[MAX];
+vector <int> v;
+
+void sieve ()
+{
+    int i,j;
+
+    v.pb(2);
+
+    for (i=3; i*i<=MAX; i+=2)
+        if (!prime[i])
+            for (j=i*i; j<MAX; j+=2*i)
+                prime[j] = true;
+
+    for (i=3; i<MAX; i+=2)
+        if (!prime[i])
+            v.pb(i);
+}
+
+ebar_khela_hoppe
+{
+    /*
+    	freopen ("input.txt","r",stdin);
+    	freopen ("output.txt","w",stdout);
+    */
+
+    sieve ();
+
+    int n,i;
+
+    while (sf ("%d",&n) != EOF)
+    {
+        for (i=0; i<n; i++)
+        {
+            pf ("%d",v[i]);
+
+            if (i == n-1)
+                pf ("\n");
+            else
+                pf (" ");
+        }
+    }
+
+    bair_ho;
+}
