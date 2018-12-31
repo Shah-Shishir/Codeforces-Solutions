538e06a4958edf6d7a31c12e5ed6ed6bd88801f8
Shah Newaj Rabbi Shishir <shahshishir.09@gmail.com>
2018-12-31 23:54:03 +0600

    Add files via upload

---
+++ 242B. Big Segment.cpp
@@ -0,0 +1,97 @@
+/***
+
+            Bismillahir Rahmanir Rahim
+            Read in the name of Allah, who created you!!!
+            Author : Shah Newaj Rabbi Shishir,
+            Department of CSE, City University, Bangladesh.
+
+***/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ebar_khela_hoppe    int main (void)
+#define bair_ho             return 0
+#define sf                  scanf
+#define pf                  printf
+#define ssf                 sscanf
+#define spf                 sprintf
+#define fsf                 fscanf
+#define fpf                 fprintf
+#define fast                ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+#define scase               sf ("%d",&tc)
+#define sn                  sf ("%d",&n)
+#define whilecase           while (tc--)
+#define eof                 while (cin >> n)
+#define forloop             for (pos=1; pos<=tc; pos++)
+#define arrayloop           for (i=0; i<n; i++)
+#define cinstr              cin >> str
+#define getstr              getline (cin,str)
+#define pcase               pf ("Case %d: ",pos)
+#define vi                  vector <int>
+#define si                  set <int>
+#define vs                  vector <string>
+#define pii                 pair <int,int>
+#define mii                 map <int,int>
+#define pb                  push_back
+#define in                  insert
+#define llu                 unsigned long long
+#define lld                 long long
+#define U                   unsigned int
+#define endl                "\n"
+
+const int MOD = 1000000007;
+const int MAX = 1000005;
+
+int SetBit (int n, int x) { return n | (1 << x); }
+int ClearBit (int n, int x) { return n & ~(1 << x); }
+int ToggleBit (int n, int x) { return n ^ (1 << x); }
+bool CheckBit (int n, int x) { return (bool)(n & (1 << x)); }
+
+ebar_khela_hoppe
+{
+    /*
+    	freopen ("input.txt","r",stdin);
+    	freopen ("output.txt","w",stdout);
+    */
+
+    int n,i,l,r;
+
+    while (sf ("%d",&n) != EOF)
+    {
+        vector <pii> v(n);
+        l = 1e9+1, r = 0;
+
+        for (i=0; i<n; i++)
+        {
+            sf ("%d %d",&v[i].first,&v[i].second);
+
+            if (v[i].first < l)
+                l = v[i].first;
+
+            if (v[i].second > r)
+                r = v[i].second;
+        }
+
+        int k = 0;
+
+        for (i=0; i<n; i++)
+        {
+            if (v[i].first == l && v[i].second == r)
+            {
+                pf ("%d",++i);
+                k = 1;
+                break;
+            }
+        }
+
+        if (!k)
+            pf ("-1");
+
+        pf ("\n");
+
+        v.clear();
+    }
+
+    bair_ho;
+}
