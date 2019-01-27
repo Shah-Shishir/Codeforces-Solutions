0dfd465791cce3a19731178c9c6845ca1925caf7
Shah Newaj Rabbi Shishir <shahshishir.09@gmail.com>
2019-01-28 04:07:58 +0600

    Add files via upload

---
+++ 450B. Jzzhu and Sequences.cpp
@@ -0,0 +1,81 @@
+/***
+
+            Bismillahir Rahmanir Rahim
+            Read in the name of Allah, who created you!!!
+            Author : Shah Newaj Rabbi Shishir,
+            Department of CSE, City University, Bangladesh.
+
+***/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ebar_khela_hoppe    int main (void)
+#define bair_ho             return 0
+#define sf                  scanf
+#define pf                  printf
+#define ssf                 sscanf
+#define spf                 sprintf
+#define fsf                 fscanf
+#define fpf                 fprintf
+#define fast                ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+#define scase               sf ("%d",&tc)
+#define sn                  sf ("%d",&n)
+#define whilecase           while (tc--)
+#define eof                 while (cin >> n)
+#define forloop             for (pos=1; pos<=tc; pos++)
+#define arrayloop           for (i=0; i<n; i++)
+#define cinstr              cin >> str
+#define getstr              getline (cin,str)
+#define pcase               pf ("Case %d: ",pos)
+#define vi                  vector <int>
+#define si		    set <int>
+#define vs                  vector <string>
+#define pii                 pair <int,int>
+#define mii                 map <int,int>
+#define pb                  push_back
+#define in                  insert
+#define llu                 unsigned long long
+#define lld                 long long
+#define U                   unsigned int
+#define endl                "\n"
+
+const int MOD = 1000000007;
+const int MAX = 1000005;
+
+int SetBit (int n, int x) { return n | (1 << x); }
+int ClearBit (int n, int x) { return n & ~(1 << x); }
+int ToggleBit (int n, int x) { return n ^ (1 << x); }
+bool CheckBit (int n, int x) { return (bool)(n & (1 << x)); }
+
+lld arr[7];
+
+ebar_khela_hoppe
+{
+    /*
+    	freopen ("input.txt","r",stdin);
+    	freopen ("output.txt","w",stdout);
+    */
+
+    lld a,b,n,ans;
+
+    while (sf ("%lld %lld\n%lld",&a,&b,&n) != EOF)
+    {
+        arr[1] = (a+MOD) % MOD;
+        arr[2] = (b+MOD) % MOD;
+        arr[3] = (b-a+MOD) % MOD;
+        arr[4] = (-a+MOD) % MOD;
+        arr[5] = (-b+MOD) % MOD;
+        arr[6] = (a-b+MOD) % MOD;
+        n %= 6;
+
+        if (n == 0)
+            n = 6;
+
+        ans = (arr[n]+MOD) % MOD;
+
+        pf ("%lld\n",ans);
+    }
+
+    bair_ho;
+}
