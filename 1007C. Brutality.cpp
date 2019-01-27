e650b092990f7e8e5d39faeacad9858b408cbe8b
Shah Newaj Rabbi Shishir <shahshishir.09@gmail.com>
2019-01-27 21:09:55 +0600

    Add files via upload

---
+++ 1007C. Brutality.cpp
@@ -0,0 +1,88 @@
+/***
+
+            Bismillahir Rahmanir Rahim
+            Read in the name of Allah, who created you!!!
+            Author : Shah Newaj Rabbi Shishir,
+            Department of CSE, City University, Bangladesh.
+
+***/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ebar_khela_hoppe    int main (void)
+#define bair_ho             return 0
+#define sf                  scanf
+#define pf                  printf
+#define ssf                 sscanf
+#define spf                 sprintf
+#define fsf                 fscanf
+#define fpf                 fprintf
+#define fast                ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+#define scase               sf ("%d",&tc)
+#define sn                  sf ("%d",&n)
+#define whilecase           while (tc--)
+#define eof                 while (cin >> n)
+#define forloop             for (pos=1; pos<=tc; pos++)
+#define arrayloop           for (i=0; i<n; i++)
+#define cinstr              cin >> str
+#define getstr              getline (cin,str)
+#define pcase               pf ("Case %d: ",pos)
+#define vi                  vector <int>
+#define si                  set <int>
+#define vs                  vector <string>
+#define pii                 pair <int,int>
+#define mii                 map <int,int>
+#define pb                  push_back
+#define in                  insert
+#define llu                 unsigned long long
+#define lld                 long long
+#define U                   unsigned int
+#define endl                "\n"
+
+const int MOD = 1000000007;
+const int MAX = 1000005;
+
+int SetBit (int n, int x) { return n | (1 << x); }
+int ClearBit (int n, int x) { return n & ~(1 << x); }
+int ToggleBit (int n, int x) { return n ^ (1 << x); }
+bool CheckBit (int n, int x) { return (bool)(n & (1 << x)); }
+
+int arr[2000005];
+char str[2000005];
+
+ebar_khela_hoppe
+{
+    /*
+    	freopen ("input.txt","r",stdin);
+    	freopen ("output.txt","w",stdout);
+    */
+
+    int n,k,i,j,x,d,p;
+
+    while (sf ("%d %d",&n,&k) != EOF)
+    {
+        for (i=0; i<n; i++)
+            sf ("%d",&arr[i]);
+
+        sf ("%s",str);
+
+        lld ans = 0;
+
+        for (i=0,j=0; i<n; i=j)
+        {
+            while (j < n && str[i] == str[j])
+                j++;
+
+            sort (arr+i,arr+j);
+            reverse (arr+i,arr+j);
+
+            for (p=i; p<min(j,i+k); p++)
+                ans += arr[p];
+        }
+
+        pf ("%lld\n",ans);
+    }
+
+    bair_ho;
+}
