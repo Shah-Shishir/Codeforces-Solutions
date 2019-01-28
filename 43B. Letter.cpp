d95c681d3d6a90dcadbd3b5fcc2828ea68d4cc86
Shah Newaj Rabbi Shishir <shahshishir.09@gmail.com>
2019-01-28 19:37:24 +0600

    Add files via upload

---
+++ 43B. Letter.cpp
@@ -0,0 +1,100 @@
+/***
+
+            Bismillahir Rahmanir Rahim
+            Read in the name of Allah, who created you!!!
+            Author : Shah Newaj Rabbi Shishir,
+            Department of CSE, City University, Bangladesh.
+
+***/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ebar_khela_hoppe    int main (void)
+#define bair_ho             return 0
+#define sf                  scanf
+#define pf                  printf
+#define ssf                 sscanf
+#define spf                 sprintf
+#define fsf                 fscanf
+#define fpf                 fprintf
+#define fast                ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+#define scase               sf ("%d",&tc)
+#define sn                  sf ("%d",&n)
+#define whilecase           while (tc--)
+#define eof                 while (cin >> n)
+#define forloop             for (pos=1; pos<=tc; pos++)
+#define arrayloop           for (i=0; i<n; i++)
+#define cinstr              cin >> str
+#define getstr              getline (cin,str)
+#define pcase               pf ("Case %d: ",pos)
+#define vi                  vector <int>
+#define si                  set <int>
+#define vs                  vector <string>
+#define pii                 pair <int,int>
+#define mii                 map <int,int>
+#define pb                  push_back
+#define in                  insert
+#define llu                 unsigned long long
+#define lld                 long long
+#define U                   unsigned int
+#define endl                "\n"
+
+const int MOD = 1000000007;
+const int MAX = 1000005;
+
+int SetBit (int n, int x) { return n | (1 << x); }
+int ClearBit (int n, int x) { return n & ~(1 << x); }
+int ToggleBit (int n, int x) { return n ^ (1 << x); }
+bool CheckBit (int n, int x) { return (bool)(n & (1 << x)); }
+
+ebar_khela_hoppe
+{
+    /*
+    	freopen ("input.txt","r",stdin);
+    	freopen ("output.txt","w",stdout);
+    */
+
+    string s1,s2;
+    int l1,l2,i,j,k,K;
+
+    while (getline(cin,s1))
+    {
+        getline(cin,s2);
+
+        l1 = s1.size();
+        l2 = s2.size();
+
+        K = true;
+
+        for (i=0; i<l2; i++)
+        {
+            if (s2[i] == ' ')
+                continue;
+
+            k = false;
+
+            for (j=0; j<l1; j++)
+            {
+                if (s1[j] == s2[i])
+                {
+                    k = true;
+                    s1[j] = '*';
+                    break;
+                }
+            }
+
+            if (!k)
+            {
+                cout << "NO" << '\n';
+                K = false;
+                break;
+            }
+        }
+
+        if (K)
+            cout << "YES" << '\n';
+    }
+
+    bair_ho;
+}
