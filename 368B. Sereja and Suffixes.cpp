79cbcb16061ae546ea5023e1124748894fc18d37
Shah Newaj Rabbi Shishir <shahshishir.09@gmail.com>
2019-10-03 02:53:36 +0600

    Add files via upload

---
+++ 368B. Sereja and Suffixes.cpp
@@ -0,0 +1,82 @@
+/***
+
+            Bismillahir Rahmanir Rahim
+            Read in the name of Allah, who created you!!!
+            Author : Shah Newaj Rabbi Shishir,
+            Department of CSE, City University, Bangladesh.
+
+***/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ebar_khela_hoppe    int main (void)
+#define bair_ho             return 0
+#define sf                  scanf
+#define pf                  printf
+#define ssf                 sscanf
+#define spf                 sprintf
+#define fsf                 fscanf
+#define fpf                 fprintf
+#define fast                ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+#define scase               sf ("%d",&tc)
+#define sn                  sf ("%d",&n)
+#define whilecase           while (tc--)
+#define eof                 while (cin >> n)
+#define forloop             for (pos=1; pos<=tc; pos++)
+#define arrayloop           for (i=0; i<n; i++)
+#define cinstr              cin >> str
+#define getstr              getline (cin,str)
+#define pcase               pf ("Case %d: ",pos)
+#define vi                  vector <int>
+#define si                  set <int>
+#define vs                  vector <string>
+#define pii                 pair <int,int>
+#define mii                 map <int,int>
+#define pb                  push_back
+#define in                  insert
+#define llu                 unsigned long long
+#define lld                 long long
+#define U                   unsigned int
+#define endl                "\n"
+
+const int MOD = 1000000007;
+const int MAX = 1000005;
+
+int SetBit (int n, int x) { return n | (1 << x); }
+int ClearBit (int n, int x) { return n & ~(1 << x); }
+int ToggleBit (int n, int x) { return n ^ (1 << x); }
+bool CheckBit (int n, int x) { return (bool)(n & (1 << x)); }
+
+ebar_khela_hoppe
+{
+    /*
+    	freopen ("input.txt","r",stdin);
+    	freopen ("output.txt","w",stdout);
+    */
+
+    int n,m,i,k;
+
+    while (sf ("%d %d",&n,&m) != EOF)
+    {
+        vi v(n+1),ans(n+1);
+        si st;
+
+        for (i=1; i<=n; i++)
+            sf ("%d",&v[i]);
+
+        for (i=n; i>=1; i--)
+        {
+            st.in(v[i]);
+            ans[i] = st.size();
+        }
+
+        for (i=1; i<=m; i++)
+        {
+            sf ("%d",&k);
+            pf ("%d\n",ans[k]);
+        }
+    }
+
+    bair_ho;
+}
