875908967edb60651cd2f0881c2d667aad560c17
Shah Newaj Rabbi Shishir <shahshishir.09@gmail.com>
2020-05-12 08:19:36 +0600

    Add files via upload

--- 1303A. Erasing Zeroes.js
+++ 1303A. Erasing Zeroes.js
@@ -1,26 +1,29 @@
 function countErasedZeroes(str) {
-    var len = str.length, start = -1, end = -1, count = 0
-    for (var i=0; i<len; i++) {
+  var len = str.length,
+      start = -1,
+      end = -1,
+      count = 0
+  for (var i = 0; i < len; i++) {
       if (str[i] === '1') {
           start = i
           break
       }
-    }
-    for (var i=len-1; i>=0; i--) {
+  }
+  for (var i = len - 1; i >= 0; i--) {
       if (str[i] === '1') {
           end = i
           break
       }
-    }
-    for (i=start; i<=end; i++) {
+  }
+  for (i = start; i <= end; i++) {
       if (str[i] === '0')
           ++count
-    }
-    return count
   }
-  
-  var tc = Number(readline())
-  while (tc--) {
-    var str = readline()
-    print(countErasedZeroes(str))
-  }
+  return count
+}
+
+var tc = Number(readline())
+while (tc--) {
+  var str = readline()
+  print(countErasedZeroes(str))
+}
